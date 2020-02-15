0a800701b9ea5f348e9bfb0067b101b32a125d43
Anil Nagar <anil@keriton.com>
2020-02-16 01:23:14 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ContentContainerComponent } from './components/content-container/content-container.component';
+import { StartPageComponent } from './components/start-page/start-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideBarComponent,
+    AboutMeComponent,
+    SkillsComponent,
+    ExperienceComponent,
+    PortfolioComponent,
+    ContactComponent,
+    ContentContainerComponent,
+    StartPageComponent
   ],
   imports: [
     BrowserModule
